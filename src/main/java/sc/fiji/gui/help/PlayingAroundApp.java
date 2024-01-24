2f387a613a1b49bf8c28512550e447492b78e6e8
Vladimír Ulman <ulman@fi.muni.cz>
2024-01-24 18:35:40 +0100

    COS: better app layout

--- src/main/java/sc/fiji/gui/help/PlayingAroundApp.java
+++ src/main/java/sc/fiji/gui/help/PlayingAroundApp.java
@@ -112,37 +112,48 @@ public class PlayingAroundApp {
 		registerComponentHelp(panel2, new URL("https://www.fi.muni.cz/~xulman/files/secret_folder/removeMe.html"));
 	}
 
+	final Color blue = new Color(128,128,255);
+	final Color green = new Color(128,192,128);
+	final Color gray = new Color(192,192,192);
+
 	void buildAppWindow() {
-		final JFrame f = new JFrame("A Simple Demo App");
+		final Panel contentPane = new Panel();
+		contentPane.setBackground(gray);
+		contentPane.setLayout(new GridBagLayout());
 
-		final Color blue = new Color(128,128,255);
-		final Color green = new Color(128,192,128);
-		final Color gray = new Color(192,192,192);
-
-		panel1 = new JButton("SIMPLE");
+		panel1 = new JButton("SIMPLE (local help)");
 		panel1.setPreferredSize(new Dimension(200,50));
 		panel1.setHorizontalAlignment(JLabel.CENTER);
 		//panel1.setBorder(BorderFactory.createEtchedBorder(blue,gray));
 		panel1.setBackground(blue);
 
-		panel2 = new JButton("APP");
+		panel2 = new JButton("APP  (remote help)");
 		panel2.setPreferredSize(new Dimension(200,50));
 		panel2.setHorizontalAlignment(JLabel.CENTER);
 		//panel2.setBorder(BorderFactory.createEtchedBorder(green,gray));
 		panel2.setBackground(green);
 
-		final GridBagLayout gridBagLayout = new GridBagLayout();
-		f.setLayout(gridBagLayout);
+		JButton panel3 = new JButton("not used");
+		panel3.setPreferredSize(new Dimension(200,50));
+		panel3.setHorizontalAlignment(JLabel.CENTER);
+		//panel3.setBorder(BorderFactory.createEtchedBorder(green,gray));
 
 		final GridBagConstraints c = new GridBagConstraints();
 		c.anchor = GridBagConstraints.CENTER;
-		c.fill = GridBagConstraints.HORIZONTAL;
+		c.fill = GridBagConstraints.BOTH;
 		c.gridx = 0;
 		c.gridy = 0;
-		f.add(panel1, c);
+		c.insets = new Insets(50,50,5,50);
+		contentPane.add(panel1, c);
 		c.gridy = 1;
-		f.add(panel2, c);
+		c.insets = new Insets(0,50,5,50);
+		contentPane.add(panel2, c);
+		c.gridy = 2;
+		c.insets = new Insets(0,50,50,50);
+		contentPane.add(panel3, c);
 
+		final JFrame f = new JFrame("A Simple Demo App");
+		f.setContentPane( contentPane );
 		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		f.pack();
 		f.setVisible(true);
