83b0784d34571f1f562b5307839d46404af61e89
Vladimír Ulman <ulman@fi.muni.cz>
2024-01-24 12:12:20 +0100

    ADD: more of the demoing..

--- src/main/java/sc/fiji/gui/help/PlayingAround.java
+++ src/main/java/sc/fiji/gui/help/PlayingAround.java
@@ -1,12 +1,22 @@
 package sc.fiji.gui.help;
 
 import javax.swing.*;
+import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 
 public class PlayingAround {
-	JComponent buildTextHtmlComponent() {
-			return null;
+	JComponent buildTextHtmlComponent(URL pathToText) {
+		JEditorPane editorPane;
+		try {
+			editorPane = new JEditorPane( pathToText );
+			editorPane.setEditable( false );
+		} catch (IOException e) {
+			System.err.println("choosing fall-back content");
+			editorPane = new JEditorPane();
+			editorPane.setText("FAILED OPENING THE CONTENT HERE.");
+		}
+		return editorPane;
 	}
 
 	JComponent buildImageOnlyComponent(URL pathToImage) {
@@ -17,14 +27,18 @@ public class PlayingAround {
 	JFrame buildPanel() {
 		JFrame f = new JFrame("PLAYING AROUND");
 
-		f.add( buildImageOnlyComponent(PlayingAround.class.getResource("someImg.png")) );
 		try {
-			f.add( buildImageOnlyComponent(new URL("https://www.fi.muni.cz/~xulman/files/secret_folder/removeMe.png")) );
+			//f.add( buildImageOnlyComponent( PlayingAround.class.getResource("someImg.png") ) );
+			//f.add( buildImageOnlyComponent( new URL("https://www.fi.muni.cz/~xulman/files/secret_folder/removeMe.png") ) );
+			//f.add( buildTextHtmlComponent( new URL("https://www.fi.muni.cz/~xulman/files/secret_folder/removeMe.html") ) );
+			buildTextHtmlComponent( new URL("https://www.fi.muni.cz/~xulman/files/secret_folder/removeMe.html") );
+			f.add( buildTextHtmlComponent( PlayingAround.class.getResource("someImg.txt") ) );
 		} catch (MalformedURLException e) {
 			System.out.println("Ain't adding an image panel 'cause URL is supposedly wrong...");
 		}
 
 		f.pack();
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		f.setVisible(true);
 		return f;
 	}
